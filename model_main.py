6480c70aaa4beccc3899d98206809dfa38cf2089
haoyuan.huhy <a7758526@gmail.com>
2015-08-01 14:45:26 +0800

    finish

--- model_main.py
+++ model_main.py
@@ -31,7 +31,7 @@ def type2id(x):
         return 0
     return -1
 
-def get_sd(id, label_start_date, fea_delta = 120):
+def get_sd(id, label_start_date, fea_delta = 240):
     # delta 指的是 确定label；的时候 是一个星期的涨幅还是什么
     # fea delta 指的是， 我要准备多久的数据
     ## 获取分笔数据
@@ -94,7 +94,7 @@ def get_trainset(sd, label_start_date, label_delta=7, is_predict=False):
     if dt_tool.is_weekend(label_start_date):
         return None
     # 这个决定了获取多久的数据作为特征
-    fea_start_date = dt_tool.add(label_start_date, -900)
+    fea_start_date = dt_tool.add(label_start_date, -60)
     trainset = sd.query('date >= "%s" and date <= "%s"'%(fea_start_date, label_start_date))
     trainset['delta'] = trainset.date.apply(lambda x : dt_tool.delta_id(x, label_start_date))
     trainset['feature'] = trainset.type  + '_' + trainset.delta
@@ -119,50 +119,55 @@ def get_trainset(sd, label_start_date, label_delta=7, is_predict=False):
         return None
     end_close = end_close.values[0]
     y = (end_close - label_close)/label_close
-    print y
-    return x, y
+    # 从数据保存的角度看， 分开保存还是太麻烦了
+    # 直接存进去吧， 名字叫label
+    x['label'] = y
+    return x
 
 # 到此为止， 获取一条训练数据的操作已经结束了
 # 在整个过程中， 随着目标时间的推移， 训练数据逐步增多
 # 那么， 让我们把上面的过程，包装成一个函数吧
 
-ss = StandardScaler()
-label_start_date = '2015-07-15'
+train_start_date = '2015-07-15'
+sd_start_date = '2015-08-01'
+pred_date = '2015-07-31'
 train_delta = -900
 id = '600588'
-sd = get_sd(id, label_start_date)
-date_list = dt_tool.dt_range(label_start_date, train_delta)
-rs = get_trainset(sd, date_list[0])
-rx, y = rs
-rx.to_csv('data/rx_1.csv', index=None)
-ry = [y]
+sd = get_sd(id, sd_start_date)
+date_list = dt_tool.dt_range(train_start_date, train_delta)
+rxy = get_trainset(sd, date_list[0])
+print date_list[0]
+rxy.to_csv('data/rxy_1.csv', index=None)
 for d in date_list:
     rs = get_trainset(sd, d)
     if rs is None:
         continue    
-    x, y = rs
-    rx = rx.append(x)
-    ry.append(y)
-rx.fillna(0, inplace=True)
-rx.to_csv('data/rx.csv', index=None)
+    rxy = rxy.append(rs)
+rxy.fillna(0, inplace=True)
+rxy.to_csv('data/rxy.csv', index=None)
 
-train_x = np.asarray(rx)
-train_y = np.asarray(ry).ravel()
-np.savetxt('data/ry.csv', train_y)
+rxy = pd.read_csv('data/rxy.csv')
+sd = pd.read_csv('data/sd.csv')
+ss = StandardScaler()
+train_x = np.asarray(rxy.drop('label', 1))
+train_y = np.asarray(rxy.label).ravel()
 train_scale_x = ss.fit_transform(train_x)
 clf = GradientBoostingRegressor()
 clf.fit(train_scale_x, train_y)
-scores = cross_val_score(clf, train_scale_x, train_y, scoring='mean_squared_error', cv=10)
+scores = cross_val_score(clf, train_scale_x, train_y, scoring='mean_squared_error', cv=3)
 print clf, math.sqrt(-scores.mean())
 
 ## 做了这么多， 我们终于拿到了一个
 ## 骑士特征还算丰富的模型
 ## 欧巴， 我们开始预测吧， 希望不会又是然并卵
 ## 可不可以， rmse降到0.03以下
-fx = get_trainset(sd, '2015-07-31', is_predict=True)
-tmpx = rx.append(fx)
+fx = get_trainset(sd, pred_date, is_predict=True)
+rxy = rxy.drop('label', 1)
+tmpx = rxy.append(fx)
 cfx = tmpx.tail(1)
 cfx.fillna(0, inplace=True)
+cfx.to_csv('data/pred_x.csv', index=None)
+cfx = pd.read_csv('data/pred_x.csv')
 pred_x = np.asarray(cfx)
 pred_scale_x = ss.fit_transform(pred_x)
 pred = clf.predict(pred_scale_x)
